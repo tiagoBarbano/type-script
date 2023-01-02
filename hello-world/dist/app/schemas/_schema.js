ab1a34d063035e5a6d618bc474e1ed388d6a66f0
tiagoBarbano <tiago.barbano@gmail.com>
2023-01-02 00:06:12 -0300

    rm dist and nodes

--- hello-world/dist/app/schemas/_schema.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const schema = { "$schema": "http://json-schema.org/draft-07/schema#", "definitions": { "UserPostRequest": { "type": "object",
-            "properties": {
-                "num_item": { "type": "number" },
-                "nome_item": { "type": "string" }
-            }, "required": ["num_item", "nome_item"] } } };
-exports.default = schema.definitions;
